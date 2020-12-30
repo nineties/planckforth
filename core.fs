a572e8571f146905a99bfb9e284db184054d6115
Koichi Nakamura <koichi@idein.jp>
2020-12-30 21:17:09 +0900

    Minor fix

--- core.fs
+++ core.fs
@@ -1,5 +1,5 @@
 h@l@h@!h@C+h!k1k0-h@$k
-h@k1k0-+!h@C+h!ih@!h@C+h!kefh@!h@C+h!l!
-h@l@h@!h@C+h!k1k0-h@$k h@k1k0-+!h@C+h!ih@!h@C+h!kefh@!h@C+h!l!
+h@k1k0-+$h@C+h!ih@!h@C+h!kefh@!h@C+h!l!
+h@l@h@!h@C+h!k1k0-h@$k h@k1k0-+$h@C+h!ih@!h@C+h!kefh@!h@C+h!l!
 
 Q
