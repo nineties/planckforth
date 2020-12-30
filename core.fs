84a53c78d7f6456e4a5ca2dc6d5376c111b9ccfb
Koichi Nakamura <koichi@idein.jp>
2020-12-30 22:00:20 +0900

    Single line comment

--- core.fs
+++ core.fs
@@ -2,4 +2,22 @@ h@l@h@!h@C+h!k1k0-h@$k
 h@k1k0-+$h@C+h!ih@!h@C+h!kefh@!h@C+h!l!
 h@l@h@!h@C+h!k1k0-h@$k h@k1k0-+$h@C+h!ih@!h@C+h!kefh@!h@C+h!l!
 
+h@
+    l@      h@!h@C+h!
+    k1k0-h@$ k\h@k1k0-+$ h@C+h!
+    i       h@!h@C+h!
+    kkf     h@!h@C+h!
+    kLf     h@!h@C+h!
+    k:k0-   h@!h@C+h!
+    k=f     h@!h@C+h!
+    kJf     h@!h@C+h!
+    k0k5-C* h@!h@C+h!
+    kef     h@!h@C+h!
+l!
+
+\ **Now we can use single-line comments!**
+
+\ planckforth -
+\ Copyright (C) 2020 nineties
+
 Q
