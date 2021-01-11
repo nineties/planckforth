04b2474681e024d641371a290b50973cf4456174
Koichi Nakamura <koichi@idein.jp>
2021-01-11 16:22:56 +0900

    Add the first benchmark

---
+++ benchmark/gendoc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir -p document
+
+FILE=document/benchmark.md
+TARGETS="i386-linux-handwritten c python"
+TIMEFORMAT='%U'
+function benchmark () {
+    sum=0
+    for i in `seq $2`; do
+        t=`{ eval $1; } 2>&1`
+        sum=`echo $t + $sum | bc`
+    done
+    average=`echo "scale=3; $sum / $2" | bc | xargs printf "%.3f"`
+    echo $average
+}
+
+function bootstrap {
+    time ./planck < bootstrap.fs benchmark/nop.fs 2>&1 > /dev/null
+}
+
+function generate-table {
+    echo "## $1"
+    echo
+    echo "| implementation | sec |"
+    echo "|:---------------|----:|"
+    for impl in $TARGETS; do
+        make $impl 2>&1 > /dev/null
+        t=`benchmark $2 $3`
+        echo "| $impl | $t |"
+    done
+}
+
+echo "# Benchmarks" >> $FILE
+generate-table "Bootstrap Time" bootstrap 5
