584491e62e960e1c8b374c9388b83ff1dea32444
Koichi Nakamura <koichi@idein.jp>
2021-12-08 08:08:13 +0900

    fix comment

--- lib/core.fs
+++ lib/core.fs
@@ -20,7 +20,7 @@ s" Invalid argument" exception constant INVALID-ARGUMENT
 s" Index out of range" exception constant OUT-OF-RANGE export
 
 defined? roll [unless]
-    : roll ( w[n-1] ... w0 n -- w0 w[n-2] ... w0 w[n-1] )
+    : roll ( w[n-1] ... w0 n -- w[n-2] ... w0 w[n-1] )
         dup 0<= if drop else swap >r 1- recurse r> swap then
     ;
 [then]
